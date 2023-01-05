c30b7459ac305dbbfeed1bd535207b45ea4feb6e
HasanovH93 <hasanovh14@gmail.com>
2023-01-05 22:28:46 +0100

    basic server init

---
+++ server/config/express.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const cors = require('../middlewares/cors');
+
+module.exports = (app) => {
+    
+    app.use(express.json());
+    app.use(cors());
+}
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const expressConfig = require('./config/express');
+const routerConfig = require('./config/routes');
+
+
+
+start();
+async function start(){
+      
+    const app = express();
+    expressConfig(app);
+    routerConfig(app);
+    
+
+ 
+    
+    
+    app.listen(3030, () => console.log('REST Service started!'))
+}
