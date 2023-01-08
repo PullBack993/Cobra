c7ee65346fc45f36ed2807590c02f5c3f183f0aa
HasanovH93 <hasanovh14@gmail.com>
2023-01-08 20:45:48 +0100

    all coins

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -2,21 +2,24 @@ const CoinGecko = require('coingecko-api');
 const fs = require('fs');
 const CoinGeckoClient = new CoinGecko();
 async function test() {
-    for (let i = 242; i <= 262; i++){
+    for (let i = 255; i <= 275; i++){
         let data = await CoinGeckoClient.coins.all({ page: i })
         console.log(data)
 
           data.data.forEach(element => {
             console.log(element.id)
-          const current = {'id': element.id, 'name': element.name}
+          const current = {'id': element.id, 'symbol': element.symbol}
           fs.appendFileSync('coins.json', JSON.stringify(current) + ',', (err)=>{console.log(err)})
             
           });
   }
 }
+
+
+
 // setTimeout(function () {
 //     test()
-// }, 60000)
+// }, 30000)
 
 const fetchCoins = require('../../coins.json')
 console.log(fetchCoins.length)
