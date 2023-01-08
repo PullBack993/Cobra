5db7abc886a4eb6ef12f249c86772be4096b6e69
HasanovH93 <hasanovh14@gmail.com>
2023-01-08 17:13:59 +0100

    update

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -1,14 +1,15 @@
 const dataController = require('express').Router();
 const CoinGecko = require('coingecko-api');
+const fs = require('fs')
 
 dataController.get('/list', async (req, res) => {
   const CoinGeckoClient = new CoinGecko();
 
+  
 
- 
-    let data = await CoinGeckoClient.coins.list();
-    console.log(data);
- 
+    let data = await CoinGeckoClient.coins.all();
+    console.log(data)
+
 });
 
 
