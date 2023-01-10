2482c987474bd7f70f4331bc227f40c85737c71e
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2023-01-10 23:09:32 +0100

    change eslint config

--- zth/src/components/DropdownInputComponent.vue
+++ zth/src/components/DropdownInputComponent.vue
@@ -1,206 +1,187 @@
 <script setup>
-import { onMounted, ref, nextTick } from "vue";
-import dataCoins from "./data/coins.json";
-import axios from "axios";
-let allCoins = dataCoins;
-let open = ref(false);
-let currentItem = ref(0);
-let activeItem = 0;
-const input = ref(null);
-const root = ref("");
-const itemList = ref(null);
-const list = ref("");
-let searchParams = ref("");
-let coin = ref(null);
+import { onMounted, ref, nextTick } from 'vue'
+import dataCoins from './data/coins.json'
+import axios from 'axios'
+const allCoins = dataCoins
+const open = ref(false)
+const currentItem = ref(null)
+let activeScrollItem = 0
+const input = ref(null)
+const root = ref('')
+const itemList = ref(null)
+const list = ref('')
+const searchParams = ref('')
+const coin = ref(null)
 
 const selectedItem = () => {
-  open.value = !open.value;
+  open.value = !open.value
   if (open.value === true) {
-    document.addEventListener("click", documentClick);
+    document.addEventListener('click', documentClick)
   } else {
-    document.removeEventListener("click", documentClick);
-    document.removeEventListener("click", documentKey);
+    document.removeEventListener('click', documentClick)
+    document.removeEventListener('click', documentKey)
   }
-};
+}
 
-function selectInput() {
-  input?.value.focus();
-
+function selectInput () {
+  input?.value.focus()
   if (open.value === false) {
-    addClickEvent();
-    open.value = true;
+    addClickEvent()
+    open.value = true
   }
 }
 
-function documentClick(event) {
+function documentClick (event) {
   if (open.value && event.target && !root.value.contains(event.target)) {
-    open.value = false;
-    document.removeEventListener("click", documentClick);
-    document.removeEventListener("click", documentKey);
+    open.value = false
+    document.removeEventListener('click', documentClick)
+    document.removeEventListener('click', documentKey)
   }
 }
 
-function addClickEvent() {
-  document.addEventListener("click", documentClick);
+function addClickEvent () {
+  document.addEventListener('click', documentClick)
 }
 
-function documentKey(event) {
+function documentKey (event) {
   if (event.keyCode === 70 && open.value === false) {
-    open.value = true;
-    event.preventDefault();
-    input?.value.focus();
-    addClickEvent();
+    open.value = true
+    event.preventDefault()
+    input?.value.focus()
+    addClickEvent()
   }
 }
-function scrollPosition(direction) {
+function scrollPosition (direction) {
   nextTick(() => {
     if (!itemList.value) {
-      return;
+      return
     }
     const items = Array.from(
-      //TODO change to ref
-      list.value.querySelectorAll(".search__container-list-items-current")
-    );
-    activeItem = Math.min(
-      Math.max(0, activeItem + direction),
+      // TODO change to ref
+      list.value.querySelectorAll('.search__container-list-items-current')
+    )
+    activeScrollItem = Math.min(
+      Math.max(0, activeScrollItem + direction),
       items.length - 1
-    );
-    const top = items[activeItem].offsetTop;
+    )
+    const top = items[activeScrollItem].offsetTop
 
-    list.value.scrollTo({ top, behavior: "smooth" });
-  });
+    list.value.scrollTo({ top, behavior: 'smooth' })
+  })
 }
-function between(a, b, c) {
-  return a > b ? c >= b && c <= a : c >= a && c <= b;
+function between (a, b, c) {
+  return a > b ? c >= b && c <= a : c >= a && c <= b
 }
 
-function isValidKeyCode(code) {
-  return between(65, 90, code) || between(97, 122, code);
+function isValidKeyCode (code) {
+  return between(65, 90, code) || between(97, 122, code)
 }
-function handleEnterEvent() {
+function handleEnterEvent () {
   if (open.value) {
-    open.value = !open.value;
+    open.value = !open.value
   }
 }
 
-function handleEscapeEvent() {
-  open.value = false;
-  input?.value.blur();
-  document.removeEventListener("click", documentClick);
+function handleEscapeEvent () {
+  open.value = false
+  input?.value.blur()
+  document.removeEventListener('click', documentClick)
 }
 
-function handleArrowDown() {
+function handleArrowDown () {
   if (open.value) {
-    currentItem.value++;
-    scrollPosition(1);
+    console.log(currentItem.value)
+    currentItem.value++
+    scrollPosition(1)
   }
 }
 
-function handleArrowUp() {
+function handleArrowUp () {
   if (open.value && currentItem.value > 0) {
-    currentItem.value--;
-    scrollPosition(-1);
+    currentItem.value--
+    console.log(currentItem.value)
+    scrollPosition(-1)
   }
 }
 
-function handleTabEvent() {
+function handleTabEvent () {
   if (!open.value) {
-    open.value = true;
+    open.value = true
   }
 }
 
-function documentKeyDown(event) {
-  if (isValidKeyCode(event.keycode)) event.preventDefault();
+function documentKeyDown (event) {
+  if (isValidKeyCode(event.keycode)) event.preventDefault()
 
   switch (event.key) {
-    case "Escape":
-      handleEscapeEvent();
-      break;
-    case "Enter":
-      handleEnterEvent();
-      break;
-    case "ArrowDown":
-      handleArrowDown();
-      break;
-    case "ArrowUp":
-      handleArrowUp();
-      break;
-    case "Tab":
-      handleTabEvent();
-      break;
+    case 'Escape':
+      handleEscapeEvent()
+      break
+    case 'Enter':
+      handleEnterEvent()
+      break
+    case 'ArrowDown':
+      handleArrowDown()
+      break
+    case 'ArrowUp':
+      handleArrowUp()
+      break
+    case 'Tab':
+      handleTabEvent()
+      break
   }
 }
 
-function onInput() {
+function onInput () {
   if (searchParams.value.length >= 3) {
     const searchedCoin = allCoins.find((coin) => {
-      console.log(coin);
+      console.log(coin)
       if (
         coin.id === searchParams.value.toLowerCase() ||
         coin.symbol === searchParams.value.toLocaleLowerCase()
       ) {
-        return coin;
+        return coin
       }
-    });
+    })
     if (searchedCoin) {
       axios
-        .post("http://localhost:3030/id", searchedCoin)
+        .post('http://localhost:3030/id', searchedCoin)
         .then((res) => {
-          coin.value = res.data;
-          console.log(res.data.binance);
+          coin.value = res.data
+          console.log(res.data.binance)
         })
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err))
     }
   }
 }
 
 onMounted(() => {
-  window.addEventListener("keydown", documentKey);
+  window.addEventListener('keydown', documentKey)
   axios
-    .post("http://localhost:3030/id", { id: "bitcoin", symbol: "btc" })
+    .post('http://localhost:3030/id', { id: 'bitcoin', symbol: 'btc' })
     .then((res) => {
-      coin.value = res.data;
-      console.log(res.data);
+      coin.value = res.data
+      console.log(res.data)
     })
-    .catch((err) => console.log(err));
-});
+    .catch((err) => console.log(err))
+})
 </script>
 
 <template>
   <div class="search__container" ref="root">
-    <input
-      @click="selectInput($event)"
-      @keydown="documentKeyDown($event)"
-      @input="onInput"
-      type="text"
-      class="search__container-input"
-      ref="input"
-      :class="open ? 'search__container-open' : 'search__container-close'"
-      v-model="searchParams"
-    />
-    <img
-      src="../assets/BaseIcons/key.svg"
-      alt="key-f"
-      class="search__container-key"
-      @click="selectInput($event)"
-    />
-    <div
-      :class="
-        open
-          ? 'search__container-dropdown'
-          : 'search__container-dropdown--is-open'
-      "
-    >
+    <input @click="selectInput($event)" @keydown="documentKeyDown($event)" @input="onInput" type="text"
+      class="search__container-input" ref="input" :class="open ? 'search__container-open' : 'search__container-close'"
+      v-model="searchParams" />
+    <img src="../assets/BaseIcons/key.svg" alt="key-f" class="search__container-key" @click="selectInput($event)" />
+    <div :class="
+      open
+        ? 'search__container-dropdown'
+        : 'search__container-dropdown--is-open'
+    ">
       <div ref="list" class="search__container-list">
         <ul v-if="coin" ref="itemList" class="search__container-list-items">
-          <li class="search__container-list-items-current">
-            {{ coin.symbol }}
-          </li>
-          <li
-            class="search__container-list-items-current"
-            v-for="(coin, index) in coin.binance"
-            :key="index"
-          >
+          <li class="search__container-list-items-current" v-for="(coin, index) in coin.binance" :currentItem="index"
+            :key="index">
             {{ coin.base }}/{{ coin.target }} {{ coin.last }}
           </li>
         </ul>
@@ -230,6 +211,7 @@ onMounted(() => {
   &-close {
     border-radius: 1rem;
   }
+
   &-key {
     width: 3rem;
     position: absolute;
@@ -238,6 +220,7 @@ onMounted(() => {
     background-color: $input-bg-dark;
     border-radius: 0.5rem;
   }
+
   &-dropdown {
     animation: topToBottom 0.35s ease-in;
     animation-fill-mode: forwards;
@@ -255,6 +238,7 @@ onMounted(() => {
       display: none;
     }
   }
+
   &-list {
     border: 0.1rem solid $input-bg-dark;
     border-top: none;
@@ -265,8 +249,7 @@ onMounted(() => {
     -webkit-tap-highlight-color: transparent;
     overflow: auto;
     overflow-x: hidden;
-    //todo ma-heidht should be 21;
-    max-height: 18rem;
+    max-height: 21rem;
     border-bottom-left-radius: 1rem;
     border-bottom-right-radius: 1rem;
 
@@ -282,16 +265,19 @@ onMounted(() => {
     }
 
     &-items {
+
       //TODO check current element and index.on match should be like hover efekt
       &-current {
         padding: 0.8rem;
         font-size: 2rem;
         color: wheat;
         list-style: none;
+
         &:hover {
           background: white;
           color: $black;
         }
+
         &-active {
           background-color: white;
           color: $black;
@@ -300,6 +286,7 @@ onMounted(() => {
     }
   }
 }
+
 .search__container-list-items-active {
   background-color: white;
   color: $black;
--- zth/vite.config.js
+++ zth/vite.config.js
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import svgLoader from "vite-svg-loader";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,20 +10,20 @@ export default defineConfig({
     vue(),
     svgLoader({
       svgoConfig: {
-        multipass: true,
-      },
-    }),
+        multipass: true
+      }
+    })
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/_global.scss";`,
-      },
-    },
-  },
-});
+        additionalData: '@import "@/assets/_global.scss";'
+      }
+    }
+  }
+})
