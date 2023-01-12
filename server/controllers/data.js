1e24953a1fb4646b7fdd5e1af53e51215d10f0a1
HasanovH93 <hasanovh14@gmail.com>
2023-01-12 17:01:34 +0100

    assign percent to market

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -9,7 +9,7 @@ dataController.post("/id", async (req, res) => {
 
   try {
     let data = await CoinGeckoClient.coins.fetch(coinName);
-    const searchedTarget = ["BTC", "ETH", "USDT", "EUR", "GBP"];
+    const searchedTarget = ["BTC", "ETH", "USDT", "EUR", "BNB"];
     if (data) {
       let coins = data.data.tickers.filter((e) =>
         searchedTarget.includes(e.target)
@@ -21,20 +21,34 @@ dataController.post("/id", async (req, res) => {
       }
 
       let uniqueData = [...new Map(binance.map((o) => [o.target, o])).values()];
-      
-     const percentageData =  data.data.market_data['price_change_24h_in_currency'];
-     const filtered = Object.entries(percentageData)
-     .filter(([key]) => ["btc", "usd", "eth", "gbp", "bnb"].includes(key))
-     .reduce((obj, [key, value]) => {
-      obj[key] = value;
-      return obj;
-    }, {});
 
-    
-      coin.image = data.data.image
-      coin.symbol = data.data.symbol
-      coin.dailyPercentage = filtered
+      const percentageData =
+        data.data.market_data["price_change_24h_in_currency"];
+      const filtered = Object.entries(percentageData)
+        .filter(([key]) => ["btc", "usd", "eth", "eur", "bnb"].includes(key))
+        .reduce((obj, [key, value]) => {
+          obj[key] = value;
+          return obj;
+        }, {});
+
+      uniqueData.forEach(function (coin) {
+        if ("usd" in filtered) {
+          filtered.usdt = filtered.usd;
+          delete filtered.usd;
+        }
+        if (Object.keys(filtered).includes(coin.target.toLowerCase())) {
+          console.log(filtered);
+          coin.percent = filtered[coin.target.toLowerCase()];
+        }
+      });
+
+      console.log(uniqueData);
+
+      coin.image = data.data.image;
+      coin.symbol = data.data.symbol;
+      coin.dailyPercentage = filtered;
       coin.market = uniqueData;
+
       res.json(coin);
     } else {
       res.json("");
