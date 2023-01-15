65999b3eabd9b86c62aaf1e049d84bd2ce3590fb
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2023-01-15 17:18:18 +0100

    adjust req from coingecko

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -1,71 +1,34 @@
 const dataController = require("express").Router();
 const CoinGecko = require("coingecko-api");
+const { SEARCH_VALUES: searchedTarget } = require("../helpers/utils");
 
 dataController.post("/id", async (req, res) => {
   const CoinGeckoClient = new CoinGecko();
   const coinSymbol = req.body.symbol.toUpperCase();
   let coinName = req.body.id;
-  let searchedTarget = [
-    "BTC",
-    "ETH",
-    "USDT",
-    "EUR",
-    "BNB",
-    "LINK",
-    "LTC",
-    "XRP",
-    "USD",
-    "FTM",
-    "AVAX",
-  ];
-  let coins = [];
 
   try {
     let data = await CoinGeckoClient.coins.fetch(coinName);
     coinName = coinName.toUpperCase();
     if (data) {
-      coins = data.data.tickers.filter((coin) => searchedTarget.includes(coin.target));
+      // Sometimes data look like target(USD) base(coinName-BTC) -- right is base base(BTC), target(USD).
+      let coins = handleReqData(data, coinSymbol);
 
-      coins.filter((marketObj, index) => {
-        if (marketObj.base !== coinSymbol) {
-          const base = marketObj.base;
-          const target = marketObj.target;
-          marketObj.base = target;
-          marketObj.target = base;
-        }
-        if (marketObj.base !== coinSymbol && marketObj.target !== coinSymbol) {
-          delete coins[index];
-          return false;
-        }
-      });
+      // Before remove duplicate,take needed coins.
+      coins = data.data.tickers.filter((coin) => searchedTarget.includes(coin.target));
       let uniqueData = removeDuplicateCoins(coins);
-      searchedTarget = searchedTarget.filter((coin) => coin !== coinSymbol.toUpperCase());
 
       const percentageData = data.data.market_data.market_cap_change_percentage_24h_in_currency;
       const priceData = data.data.market_data.current_price;
       const filterPercentage = Object.entries(percentageData).filter(([key]) =>
         searchedTarget.includes(key.toUpperCase())
       );
-      const filterPriceData = Object.entries(priceData).filter(([key]) => 
-         searchedTarget.includes(key.toUpperCase())
+      const filterPriceData = Object.entries(priceData).filter(([key]) =>
+        searchedTarget.includes(key.toUpperCase())
       );
+      // Preparation for send response.
+      let resData = createResponseObj(uniqueData, filterPercentage, filterPriceData);
 
-      let resData = Object.entries(uniqueData).map(([key, { target, base }]) => {
-        console.log(target)
-        let [, percentage] = filterPercentage.find((k) => k[0] === target.toLowerCase()) || [];
-        let [, price] =
-          filterPriceData.find((k) => k[0] === target.toLowerCase()) || [];
-        console.log(price)
-        if (!!price) {
-          return {
-            price,
-            percentage,
-            base,
-            target,
-          };
-        }
-      });
-      console.log(resData.length <= 1)
       if (resData.length <= 1) {
         let price = data.data?.tickers[0]?.converted_last?.usd;
         return res.json({
@@ -73,27 +36,62 @@ dataController.post("/id", async (req, res) => {
           price,
           target: "usd",
           image: data.data.image,
-          name: coinName.substring(0, 1).toUpperCase() + coinName.substring(1),
+          name: coinName.substring(0, 1) + coinName.substring(1),
         });
       }
       res.json({
         ...resData,
         image: data.data.image,
-        name: coinName.toLowerCase().substring(0, 1).toUpperCase() + coinName.substring(1),
+        name: coinName.toLowerCase().substring(0, 1) + coinName.substring(1),
       });
     } else {
       res.json("");
     }
   } catch (error) {
     console.log(error);
-      res.json("");
-
+    res.json("error", error);
   }
 });
 
+function createResponseObj(uniqueData, filterPercentage, filterPriceData) {
+  let responseData = Object.entries(uniqueData).map(([key, { target, base }]) => {
+    let [, percentage] = filterPercentage.find((k) => k[0] === target.toLowerCase()) || [];
+    let [, price] = filterPriceData.find((k) => k[0] === target.toLowerCase()) || [];
+    if (!!price) {
+      return {
+        price,
+        percentage,
+        base,
+        target,
+      };
+    }
+  });
+  return responseData;
+}
+
+function handleReqData(data, inputCoinSymbol) {
+  let coins = data.data.tickers.filter((coinsObj, index) => {
+    if (coinsObj.base !== inputCoinSymbol) {
+      const base = coinsObj.base;
+      const target = coinsObj.target;
+      coinsObj.base = target;
+      coinsObj.target = base;
+      return coinsObj;
+    }
+    if (coinsObj.base !== inputCoinSymbol && coinsObj.target !== inputCoinSymbol) {
+      delete data.data.tickers[index];
+      return false;
+    }
+  });
+  return coins;
+}
+
 function removeDuplicateCoins(coins) {
   let uniqueData = coins.filter(
-    (coin, index) => coins.map((c) => c.base + (c.target = c.target === "USDT" ? "USD" : c.target)).indexOf(coin.base + coin.target) === index
+    (coin, index) =>
+      coins
+        .map((c) => c.base + (c.target = c.target === "USDT" ? "USD" : c.target))
+        .indexOf(coin.base + coin.target) === index
   );
   return uniqueData;
 }
