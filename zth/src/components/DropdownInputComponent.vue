85a1137bb78830d38996344123b988824147dd3c
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2023-01-16 00:57:51 +0100

    adjust search ui

--- zth/src/components/DropdownInputComponent.vue
+++ zth/src/components/DropdownInputComponent.vue
@@ -1,177 +1,203 @@
 <script setup>
-import { onMounted, ref, nextTick } from 'vue'
-import dataCoins from './data/coins.json'
-import axios from 'axios'
+import { onMounted, ref, nextTick } from 'vue';
+import dataCoins from './data/coins.json';
+import axios from 'axios';
 import HorizontalEllipsisSpinner from './utils/HorizontalEllipsisSpinner.vue';
-const allCoins = dataCoins
-const open = ref(false)
-const currentItem = ref(null)
-let activeScrollItem = 0
-const input = ref(null)
-const root = ref('')
-const itemList = ref(null)
-const list = ref('')
-const searchParams = ref('')
-const coin = ref(null)
-let timeout = ref(null)
+const allCoins = dataCoins;
+const open = ref(false);
+const currentItem = ref(null);
+let activeScrollItem = 0;
+const input = ref(null);
+const root = ref('');
+const itemList = ref(null);
+const list = ref('');
+const searchParams = ref('');
+const coins = ref(null);
+let timeout = ref(0);
+let loading = ref(false);
 
 const selectedItem = () => {
-  open.value = !open.value
+  open.value = !open.value;
   if (open.value === true) {
-    document.addEventListener('click', documentClick)
+    document.addEventListener('click', documentClick);
   } else {
-    document.removeEventListener('click', documentClick)
-    document.removeEventListener('click', documentKey)
+    document.removeEventListener('click', documentClick);
+    document.removeEventListener('click', documentKey);
   }
-}
+};
 
 function selectInput() {
-  input?.value.focus()
+  input?.value.focus();
   if (open.value === false) {
-    addClickEvent()
-    open.value = true
+    addClickEvent();
+    open.value = true;
   }
 }
 
 function documentClick(event) {
   if (open.value && event.target && !root.value.contains(event.target)) {
-    open.value = false
-    document.removeEventListener('click', documentClick)
-    document.removeEventListener('click', documentKey)
+    open.value = false;
+    document.removeEventListener('click', documentClick);
+    document.removeEventListener('click', documentKey);
   }
 }
 
 function addClickEvent() {
-  document.addEventListener('click', documentClick)
+  document.addEventListener('click', documentClick);
 }
 
 function documentKey(event) {
   if (event.keyCode === 70 && open.value === false) {
-    open.value = true
-    event.preventDefault()
-    input?.value.focus()
-    addClickEvent()
+    open.value = true;
+    event.preventDefault();
+    input?.value.focus();
+    addClickEvent();
   }
 }
 function scrollPosition(direction) {
   nextTick(() => {
     if (!itemList.value) {
-      return
+      return;
     }
     const items = Array.from(
       // TODO change to ref
       list.value.querySelectorAll('.search__container-list-items-current')
-    )
+    );
     activeScrollItem = Math.min(
       Math.max(0, activeScrollItem + direction),
       items.length - 1
-    )
-    const top = items[activeScrollItem].offsetTop
+    );
+    const top = items[activeScrollItem].offsetTop;
 
-    list.value.scrollTo({ top, behavior: 'smooth' })
-  })
+    list.value.scrollTo({ top, behavior: 'smooth' });
+  });
 }
 function between(a, b, c) {
-  return a > b ? c >= b && c <= a : c >= a && c <= b
+  return a > b ? c >= b && c <= a : c >= a && c <= b;
 }
 
 function isValidKeyCode(code) {
-  return between(65, 90, code) || between(97, 122, code)
+  return between(65, 90, code) || between(97, 122, code);
 }
 function handleEnterEvent() {
   if (open.value) {
-    open.value = !open.value
+    open.value = !open.value;
   }
 }
 
 function handleEscapeEvent() {
-  open.value = false
-  input?.value.blur()
-  document.removeEventListener('click', documentClick)
+  open.value = false;
+  input?.value.blur();
+  document.removeEventListener('click', documentClick);
 }
 
 function handleArrowDown() {
   if (open.value) {
     // console.log(currentItem.value)
-    currentItem.value++
-    scrollPosition(1)
+    currentItem.value++;
+    scrollPosition(1);
   }
 }
 
 function handleArrowUp() {
   if (open.value && currentItem.value > 0) {
-    currentItem.value--
+    currentItem.value--;
     // console.log(currentItem.value)
-    scrollPosition(-1)
+    scrollPosition(-1);
   }
 }
 
 function handleTabEvent() {
   if (!open.value) {
-    open.value = true
+    open.value = true;
   }
 }
 
 function documentKeyDown(event) {
-  if (isValidKeyCode(event.keycode)) event.preventDefault()
+  if (isValidKeyCode(event.keycode)) event.preventDefault();
 
   switch (event.key) {
     case 'Escape':
-      handleEscapeEvent()
-      break
+      handleEscapeEvent();
+      break;
     case 'Enter':
-      handleEnterEvent()
-      break
+      handleEnterEvent();
+      break;
     case 'ArrowDown':
-      handleArrowDown()
-      break
+      handleArrowDown();
+      break;
     case 'ArrowUp':
-      handleArrowUp()
-      break
+      handleArrowUp();
+      break;
     case 'Tab':
-      handleTabEvent()
-      break
+      handleTabEvent();
+      break;
   }
 }
 
 function onInput() {
-  clearTimeout(timeout.value)
-  timeout.value = setTimeout(() => {
-    // let searcedCoinIndex = 0
-    if (searchParams.value.length >= 3) {
-      let searchedCoin = allCoins.find((coin, index) => {
-        if (
-          coin?.id === searchParams.value.toLowerCase() ||
-          coin?.symbol === searchParams.value.toLocaleLowerCase()
-        ) {
-          return coin
-        }
-      })
+  loading.value = true;
 
-      if (searchedCoin) {
-        axios
-          .post('http://localhost:3030/id', searchedCoin)
-          .then((res) => {
-            console.log(res)
-          })
-          .catch((err) => console.log(err))
-      }
+  clearTimeout(timeout?.value);
+  try {
+    if (timeout?.value === 0 || searchParams?.value.length <= 2) {
+      loading.value = false;
     }
-  }, 500)
+    timeout.value = setTimeout(() => {
+      if (searchParams?.value.length >= 3) {
+        let searchedCoin = allCoins.find((coin, index) => {
+          if (
+            coin?.id === searchParams.value.toLowerCase() ||
+            coin?.symbol === searchParams.value.toLocaleLowerCase()
+          ) {
+            return coin;
+          }
+        });
+        console.log(searchedCoin);
+
+        if (searchedCoin) {
+          axios
+            .post('http://localhost:3030/id', searchedCoin)
+            .then((res) => {
+              coins.value = res.data;
+              loading.value = false;
+
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err);
+              loading.value = false;
+            });
+        } else {
+          loading.value = false;
+          coins.value = '';
+        }
+      }
+    }, 500);
+  } catch (err) {
+    console.log(err);
+    loading.value = false;
+  }
 }
 
 onMounted(() => {
-  window.addEventListener('keydown', documentKey)
+  window.addEventListener('keydown', documentKey);
   timeout.value = setTimeout(() => {
+    loading.value = true;
+
     axios
       .post('http://localhost:3030/id', { id: 'bitcoin', symbol: 'btc' })
       .then((res) => {
-        coin.value = res.data
-        console.log(coin.value)
+        console.log(res.data);
+        coins.value = res.data;
+        loading.value = false;
       })
-      .catch((err) => console.log(err))
-  }, 500)
-})
+      .catch((err) => {
+        console.log(err);
+        loading.value = false;
+      });
+  }, 500);
+});
 </script>
 
 <template>
@@ -199,24 +225,48 @@ onMounted(() => {
           : 'search__container-dropdown--is-open'
       "
     >
-      <div ref="list" class="search__container-list">
-        <!-- <ul v-if="coin" ref="itemList" class="search__container-list-items">
-          <div style="color: white">
-            <img :src="coin.image.thumb" alt="" />{{
-              coin.symbol.toUpperCase()
-            }}
-            Price
+      <div ref="list" class="search__container-list" v-if="coins && !loading">
+        <ul ref="itemList" class="search__container-list-items">
+          <div class="search__container-list-container">
+            <img
+              class="search__container-list-image"
+              loading="lazy"
+              :src="coins.image.small"
+              :alt="coins.name"
+            />
+            <div class="search__container-list-coin-name">
+              {{ coins.name }}
+            </div>
           </div>
+          <div class="search__container-list-line-top"></div>
           <li
-            class="search__container-list-items-current"
-            v-for="(coin, index) in coin.market"
+            class="search__container-list-current"
+            v-for="(coin, index) in coins.data"
             :currentItem="index"
             :key="index"
           >
-            {{ coin.base }}/{{ coin.target }} {{ coin.last }} -
-            {{ ((coin.percent / coin.last) * 100).toFixed(2) }}%
+            <div class="search__container-list-current--base">
+              {{ coin?.base }}/{{ coin?.target }}
+            </div>
+            <div class="search__container-list-current--price">
+              {{ coin?.price }}
+            </div>
+            <div class="search__container-list-current--percentage">
+              {{ coin?.percentage }}%
+            </div>
+            <div class="search__container-list-line"></div>
           </li>
-        </ul> -->
+        </ul>
+      </div>
+      <div v-if="loading" class="search__container-spinner">
+        <HorizontalEllipsisSpinner />
+      </div>
+      <!-- TODO set class/style -->
+      <div
+        v-if="!coins && !loading && searchParams.length > 0"
+        class="search__container-list-current--base"
+      >
+        No results for "{{ searchParams }}"
       </div>
     </div>
   </div>
@@ -296,31 +346,79 @@ onMounted(() => {
       background-clip: padding-box;
     }
 
-    &-items {
-      //TODO check current element and index.on match should be like hover efekt
-      &-current {
-        padding: 0.8rem;
-        font-size: 2rem;
-        color: wheat;
-        list-style: none;
+    &-container {
+      display: flex;
+      min-height: 2.4rem;
+    }
+    &-image {
+      width: 2rem;
+      min-width: 2rem;
+      margin: 0.2rem;
+      margin-left: 1rem;
+    }
 
-        &:hover {
-          background: white;
-          color: $black;
-        }
+    &-coin-name {
+      margin-left: 1rem;
+      display: flex;
+      align-items: center;
+      color: white;
+      font-weight: bold;
+    }
 
-        &-active {
-          background-color: white;
-          color: $black;
-        }
+    &-line {
+      width: 95%;
+      border-bottom: 0.1rem solid $main-purple-dark;
+      margin: auto;
+      padding: 1rem;
+      display: flex;
+      justify-content: space-between;
+      position: absolute;
+//TODO check it
+      &:not(:last-child) {
+        border-bottom: none;
+      }
+
+      &-top {
+        width: 95%;
+        border-bottom: 0.1rem solid $main-purple-dark-5;
+        margin: auto;
+        padding-top: 0.5rem;
+      }
+    }
+
+    //TODO check current element and index.on match should be like hover efekt
+    &-current {
+      font-size: 2rem;
+      color: wheat;
+      list-style: none;
+      display: flex;
+      justify-content: space-between;
+      &--base,
+      &--price,
+      &--percentage {
+        font-weight: 600;
+        font-size: 1.4rem;
+        line-height: 1.71429;
+        text-overflow: ellipsis;
+      }
+      &--price {
+      }
+
+      &:hover {
+        background: white;
+        color: $black;
+      }
+
+      &-active {
+        background-color: white;
+        color: $black;
       }
     }
   }
-}
-
-.search__container-list-items-active {
-  background-color: white;
-  color: $black;
+  &-spinner {
+    display: flex;
+    justify-content: center;
+  }
 }
 
 @keyframes topToBottom {
@@ -332,4 +430,6 @@ onMounted(() => {
     opacity: 1;
   }
 }
+
+//TODO responsive searchbar after implement login/register
 </style>
