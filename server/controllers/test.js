847b7ff00716b9da0a59fe423b4e9c32fecc5e64
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2023-01-21 16:14:41 +0100

    improve request fetch data from coin gecko

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -1,26 +1,44 @@
-const fs = require('fs');
+const fs = require("fs");
 const CoinGecko = require("coingecko-api");
 
 const CoinGeckoClient = new CoinGecko();
-
-
+let counter = 0;
 
 async function test() {
-    for (let i = 253; i <= 270; i++){
-        let data = await CoinGeckoClient.coins.all({ page: i })
-        console.log(data)
+  let max = 10;
+  for (let i = 1; i <= max; i++) {
+    let data = await CoinGeckoClient.coins.all({ page: i });
+    console.log(counter);
+    counter++;
+    if (!data.data) {
+            await wait();
+    }
 
-          data.data.forEach(element => {
-            console.log(element.id)
-          const current = {'id': element.id, 'symbol': element.symbol}
-          fs.appendFileSync('coins.json', JSON.stringify(current) + ',', (err)=>{console.log(err)})
+    data.data.forEach((element) => {
+      console.log(element.id);
+      const current = { id: element.id, symbol: element.symbol };
+      fs.appendFileSync("coins.json", JSON.stringify(current) + ",", (err) => {
+        console.log(err);
+      });
+    });
 
-          });
+    if (i % 10 === 0) {
+      max += 10;
+      await wait()
+      console.log(max)
+    }
   }
 }
 
+async function wait(seconds) {
+  console.log("waitttttttttttttttttttttt", new Date().toLocaleTimeString());
+  await new Promise((resolve) => setTimeout(resolve, seconds | 60000));
+  console.log("gooooooooooooooo ", new Date().toLocaleTimeString());
+  
+  
+}
 
 //TODO take all icon also and set it to search => example img=USD => BTC/USD, img=link BTC/LINK
-
-const fetchCoins = require('../../coins.json')
-console.log(fetchCoins.length)
+test();
+// const fetchCoins = require('../../coins.json')
+// console.log(fetchCoins.length)
