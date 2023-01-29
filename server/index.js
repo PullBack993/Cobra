088a921e2fb9f9a0c992456ae184756e96ec662b
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2023-01-29 16:27:40 +0100

    workaround implementation login(metamask)

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const expressConfig = require('./config/express');
-const routerConfig = require('./config/routes');
+require("dotenv/config");
+const express = require("express");
+const databaseConfig = require("./config/database");
+const expressConfig = require("./config/express");
+const routerConfig = require("./config/routes");
 
 start();
 async function start() {
   const app = express();
   expressConfig(app);
+  await databaseConfig(app);
   routerConfig(app);
 
-  app.listen(3030, () => console.log('REST Service started on port 3030'));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`App start ==> http://localhost:3000`));
 }
