b1db0529f5b680cfeba5eaa3d78b29c68a462a00
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2023-02-20 08:00:24 +0100

    fix some issue with eslint config

--- zth/.eslintrc.js
+++ zth/.eslintrc.js
@@ -1,6 +1,3 @@
-const _require = require('esm')(module);
-module.exports = _require('./.eslintrc.esm.js').default;
-
 module.exports = {
   root: true,
   env: {
@@ -8,6 +5,7 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-strongly-recommended',
+    'plugin:@typescript-eslint/recommended',
     '@vue/airbnb',
     'prettier',
     '@vue/typescript',
@@ -15,6 +13,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    parser: '@typescript-eslint/parser',
   },
   ecmaFeatures: {
     jsx: false,
@@ -23,23 +22,30 @@ module.exports = {
     'import/resolver': {
       node: {
         extensions: ['.js', '.ts', '.json', '.vue'],
+        moduleDirectory: ['.', 'node_modules'],
       },
       alias: {
         map: [['@', './src']],
-        extensions: ['.ts', '.js', '.vue', '.jsx'],
+        extensions: ['.ts', '.js', '.vue'],
       },
     },
   },
   rules: {
+    'vue/block-lang': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     semi: ['error', 'always'],
     quotes: ['error', 'single'],
-    indent: ['error', 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'prefer-const': ['error', { destructuring: 'all' }],
     'vuejs-accessibility/click-events-have-key-events': 'off',
     'vuejs-accessibility/label-has-for': 'off',
-    'comma-spacing': ['error', { before: false, after: true }],
     'import/prefer-default-export': 'off',
+    'no-plusplus': 'off',
+    'no-const-assign': 1,
+    eqeqeq: 1,
   },
   globals: {
     $: true,
