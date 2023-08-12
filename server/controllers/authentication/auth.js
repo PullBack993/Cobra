25ed135d573d9f4102fe5506d90382f0ca879465
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2023-08-12 15:05:47 +0200

    fix issue with web3

--- server/controllers/authentication/auth.js
+++ server/controllers/authentication/auth.js
@@ -1,5 +1,5 @@
 require("dotenv/config");
-const Web3 = require("web3");
+const { Web3 } = require("web3");
 const https = require("https");
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
@@ -16,9 +16,9 @@ router.get("/", authenticateToken, async (req, res) => {
 router.post("/meta-mask", async (req, res) => {
   try {
     const address = req.body.address;
-    // let balance = await getBalance(address); // TODO new Web error
+    let balance = await getBalance(address); // TODO new Web error
     // if (!balance) balance = 0;
-    let balance = 0;
+    // let balance = 0;
     const userData = await getIpData();
     const user = await UserMetaMask.findOne({ ethHash: address });
     console.log("user", user);
@@ -70,7 +70,7 @@ function createToken(userId) {
 }
 
 async function getBalance(address) {
-  const web3 = await Web3.providers.HttpProvider(process.env.WEB3);
+  const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3));
   if (address) {
     const balance = await web3.eth.getBalance(address);
     return web3.utils?.fromWei(balance, "ether");
